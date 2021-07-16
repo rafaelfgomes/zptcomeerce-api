b9e885e525f7017cca63dcf71b3ce5a76d3c4573
Rafael Gomes <faelfg@gmail.com>
2021-07-15 23:45:17 -0300

    Created handlers for errors and exceptions

--- api/app/Exceptions/Handler.php
+++ api/app/Exceptions/Handler.php
@@ -2,11 +2,30 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Error;
+use TypeError;
 use Throwable;
+use ErrorException;
+use LogicException;
+use ReflectionException;
+use BadMethodCallException;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Response;
+use Psy\Exception\FatalErrorException;
+use Illuminate\Database\QueryException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Database\Eloquent\RelationNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -37,5 +56,62 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof NotFoundHttpException) {
+            return $this->errorResponse('Não encontrado', Response::HTTP_NOT_FOUND);
+        }
+
+        if ($e instanceof ModelNotFoundException) {
+            return $this->errorResponse('Não encontrado', Response::HTTP_NOT_FOUND);
+        }
+
+        if ($e instanceof AuthorizationException) {
+            return $this->errorResponse($e->getMessage(), Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($e instanceof AuthenticationException) {
+            return $this->errorResponse($e->getMessage(), Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($e instanceof ValidationException) {
+            $errors = [
+                'message' => 'Dados inválidos',
+                'errors' => $e->validator->errors()
+            ];
+            
+            return $this->errorsResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if ($e instanceof HttpException) {
+            $code = $e->getStatusCode();
+
+            return $this->errorResponse(Response::$statusTexts[$code], $code);
+        }
+
+        if($e instanceof Error) {
+            return $this->errorResponse($e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($e instanceof ReflectionException ||
+            $e instanceof QueryException ||
+            $e instanceof BadMethodCallException ||
+            $e instanceof FatalErrorException ||
+            $e instanceof ErrorException ||
+            $e instanceof LogicException ||
+            $e instanceof RelationNotFoundException ||
+            $e instanceof TypeError
+        ) {
+
+            return $this->errorResponse($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        if (env('APP_DEBUG', false)) {
+            return parent::render($request, $e);
+        }
+
+        return $this->errorResponse('Erro inesperado', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
